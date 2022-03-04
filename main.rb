59f16dc1a9f78d1a0488325d6758fdc051ed0b5e
salimabdulai <maouslayer@gmail.com>
2022-03-04 12:47:40 +0000

    Add bugs

--- main.rb
+++ main.rb
@@ -4,9 +4,9 @@ require_relative './classroom'
 require_relative './book'
 require_relative './rental'
 
-@books = []
-@people = []
-@rentals = []
+books = []
+people = []
+rentals = []
 
 def input_number(limit, statement = '')
   input = nil
@@ -50,20 +50,20 @@ def choose_y_n(statement = '')
 end
 
 def list_all_books(with_index: false)
-  return @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" } unless with_index == true
+  return books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" } unless with_index == true
 
-  @books.each_with_index do |book, index|
+  books.each_with_index do |book, index|
     puts "#{index + 1}) Title: #{book.title}, Author: #{book.author}"
   end
 end
 
 def list_all_people(with_index: false)
   if with_index == true
-    @people.each_with_index do |person, index|
+    people.each_with_index do |person, index|
       puts "#{index + 1}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   else
-    @people.each do |person|
+    people.each do |person|
       puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
@@ -72,7 +72,7 @@ end
 def list_rentals_for_id
   person_id = input_number(400, 'ID of person')
   puts 'Rentals'
-  @rentals.map do |rental|
+  rentals.map do |rental|
     puts "Date: #{rental.date} , Book: #{rental.book.title} by #{rental.book.author}" if rental.person.id == person_id
   end
 end
@@ -82,7 +82,7 @@ def create_student
   name = input_string_no_num(1, 'Name')
   has_parental_permission = choose_y_n('Has parent permission?')
   new_person = Student.new(nil, age, name, parent_permission: has_parental_permission)
-  @people << new_person
+  people << new_person
 end
 
 def create_teacher
@@ -90,7 +90,7 @@ def create_teacher
   name = input_string_no_num(1, 'Name')
   specialization = input_string(1, 'Specialization')
   new_person = Teacher.new(specialization, age, name)
-  @people << new_person
+  people << new_person
 end
 
 def create_person
@@ -112,28 +112,28 @@ def create_book
   author = input_string_no_num(1, 'Author')
   new_book = Book.new(title, author)
   puts 'Book created successfully'
-  @books << new_book
+  books << new_book
 end
 
 def create_rental
-  return puts 'No books available' if @books.empty?
-  return puts 'No person available' if @people.empty?
+  return puts 'No books available' if books.empty?
+  return puts 'No person available' if people.empty?
 
   puts 'Select a book from the following list of numbers'
   list_all_books(with_index: true)
 
-  book_index = input_number(@books.length)
-  book = @books[book_index - 1]
+  book_index = input_number(books.length)
+  book = books[book_index - 1]
 
   puts 'Select a person from the following list of numbers (not_id)'
   list_all_people(with_index: true)
-  person_index = input_number(@people.length)
-  person = @people[person_index - 1]
+  person_index = input_number(people.length)
+  person = people[person_index - 1]
 
   date = input_string(4, 'Date')
 
   new_rental = Rental.new(person, book, date)
-  @rentals << new_rental
+  rentals << new_rental
   puts 'Rental created successfully'
 end
 
@@ -176,7 +176,7 @@ def main
     display_menu
     option = input_number(7)
     is_running = false if option == 7
-    display_choice(option)
+    display_choice
   end
 end
 
