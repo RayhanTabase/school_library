06348b87a83143051b347100a4828c8395ed91fb
Rayhan <maouslayer@gmail.com>
2022-05-11 20:42:36 +0000

    Add person,student,teacher classes

--- corrector.rb
+++
@@ -1,9 +0,0 @@
-class Corrector
-  def correct_name(name)
-    name = name.capitalize
-
-    return name[0...10] if name.length > 10
-
-    name
-  end
-end
--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './corrector'
 
 class Person
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age
 
   attr_reader :id
 
@@ -10,8 +10,6 @@ class Person
     @name = name
     @parent_permission = parent_permission
     @age = age
-    @corrector = Corrector.new
-    @rentals = []
   end
 
   def of_age?
@@ -20,14 +18,6 @@ class Person
 
   def can_use_services?
     @parent_permission || is_of_age?
-  end
-
-  def validate_name
-    @name = @corrector.correct_name(@name)
-  end
-
-  def add_rental(rental)
-    @rentals << rental
   end
 
   private :of_age?
